1a8b0b4a0e114af94f8b321c34b3d504a6a424fc
koshmanton <63317462+koshmanton@users.noreply.github.com>
2023-04-06 21:30:03 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,10 +2,11 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 959806937 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    sum = 0
+    for i in range(len(x)):
+      sum += x[i]
+    sr = sum / len(x)
+    return 1 / (sr+34)
